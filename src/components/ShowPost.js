45131a10a4e382f361981e2b796256c15b902cb7
Kunal-deve1oper <ghoshkunal106@gmail.com>
2022-10-16 23:22:00 +0530

    feedback updated

--- src/components/ShowPost.js
+++ src/components/ShowPost.js
@@ -1,18 +1,20 @@
 import React from 'react'
 
 export default function ShowPost(props) {
+
+
     return (
-        <div className="my-3 card" key={props.index}>
+        <div className="my-3 card">
             <div className="card-body">
-                <h5 className="card-title">{`Post #${props.index + 1}`}</h5>
-                <p className="card-text"><strong>${props.element}</strong></p>
-                <button id={`${props.index + 1}`} onClick={() => { props.deletePost(this.id) }} className="btn btn-danger">Delete Post</button>
+                <h5 className="card-title">{`Post #${props.id}`}</h5>
+                <p className="card-text"><strong>{props.postContent}</strong></p>
+                <button className="btn btn-danger" onClick={()=>{props.deletePost(props.id)}}>Delete Post</button>
                 <div>
                     <p className="my-2">Your comments :</p>
                 </div>
                 <div className="my-3 form-floating">
                     <textarea className="form-control" placeholder="Leave a comment here" id="floatingTextarea"></textarea>
-                    <label for="floatingTextarea">Write a comment</label>
+                    <label htmlFor="floatingTextarea">Write a comment</label>
                 </div>
                 <button className="btn btn-secondary">Post comment</button>
             </div>
