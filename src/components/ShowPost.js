ab6caec8baeadcf087c4bb1a6b8b1ce0ec5ed223
Kunal-deve1oper <ghoshkunal106@gmail.com>
2022-10-21 22:52:21 +0530

    comments functionality added

--- src/components/ShowPost.js
+++ src/components/ShowPost.js
@@ -1,6 +1,11 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function ShowPost(props) {
+
+
+    const handleChange = (e)=>{
+        props.setComment(e.target.value);
+    }
 
 
     return (
@@ -11,12 +16,15 @@ export default function ShowPost(props) {
                 <button className="btn btn-danger" onClick={()=>{props.deletePost(props.id)}}>Delete Post</button>
                 <div>
                     <p className="my-2">Your comments :</p>
+                    {props.showComments.map((e,i)=>{
+                        return <p key={i}>{e}</p>
+                    })}
                 </div>
                 <div className="my-3 form-floating">
-                    <textarea className="form-control" placeholder="Leave a comment here" id="floatingTextarea"></textarea>
+                    <textarea className="form-control" placeholder="Leave a comment here" id="floatingTextarea" onChange={handleChange} value={props.comment}></textarea>
                     <label htmlFor="floatingTextarea">Write a comment</label>
                 </div>
-                <button className="btn btn-secondary">Post comment</button>
+                <button className="btn btn-secondary" onClick={()=>{props.addComments(props.id)}}>Post comment</button>
             </div>
         </div>
     )
