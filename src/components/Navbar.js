3bccf036b4f5146759b709423435f6ba9aa8d26d
Kunal-deve1oper <ghoshkunal106@gmail.com>
2023-01-02 12:55:23 +0530

    fixed login func

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,25 +1,65 @@
-import React from 'react'
+import React from "react";
+import { Link,useNavigate } from "react-router-dom";
 
 function Navbar(props) {
-    return (
-        <nav className="navbar navbar-dark bg-dark navbar-expand-lg bg-light">
-            <div className="container-fluid">
-                <a className="navbar-brand" href="/">{props.title}</a>
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
+
+    const navigate = useNavigate();
+
+    const logout =()=>{
+        localStorage.removeItem("isLogged");
+        localStorage.removeItem("username");
+        navigate("/login");
+        props.logged(true);
+    }
+
+
+  return (
+    <nav className="navbar navbar-dark bg-dark navbar-expand-lg bg-light">
+      <div className="container-fluid">
+        <Link className="navbar-brand" to="/">
+          {props.title}
+        </Link>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <Link className="nav-link" aria-current="page" to="/">
+                Home
+              </Link>
+            </li>
+          </ul>
+          {!localStorage.getItem("isLogged") ? (
+            <>
+              <Link to="/signup">
+                <button type="button" className="btn btn-primary mx-3">
+                  Sign Up
                 </button>
-                <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                        <li className="nav-item">
-                            <a className="nav-link" aria-current="page" href="/">Home</a>
-                        </li>
-                    </ul>
-                    <button type="button" className="btn btn-primary mx-3">Sign Up</button>
-                    <button type="button" className="btn btn-primary">Login</button>
-                </div>
-            </div>
-        </nav>
-    )
+              </Link>
+              <Link to="/login">
+                <button type="button" className="btn btn-primary">
+                  Login
+                </button>
+              </Link>
+            </>
+          ) : (<>
+            <div className="text-light mx-3">Welcome {localStorage.getItem("username")}</div>
+            <button type="button" className="btn btn-primary" onClick={logout}>Logout</button>
+            </>
+          )}
+        </div>
+      </div>
+    </nav>
+  );
 }
 
-export default Navbar
+export default Navbar;
